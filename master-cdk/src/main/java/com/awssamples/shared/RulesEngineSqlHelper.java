c7e61a162c731edbcde519caef6dde152f2cb992
Tim Mattison <tim@mattison.org>
2020-10-14 17:04:26 -0400

    Added more IoT rule functions

--- master-cdk/src/main/java/com/awssamples/shared/RulesEngineSqlHelper.java
+++ master-cdk/src/main/java/com/awssamples/shared/RulesEngineSqlHelper.java
@@ -31,6 +31,53 @@ public class RulesEngineSqlHelper {
         return new CfnTopicRule(stack, topicRuleName, cfnTopicRuleProps);
     }
 
+    public static CfnTopicRule buildIotEventRule(Stack stack, String topicRuleName, CfnTopicRule.RepublishActionProperty republishActionProperty, String sql) {
+        CfnTopicRule.ActionProperty actionProperty = CfnTopicRule.ActionProperty.builder()
+                .republish(republishActionProperty)
+                .build();
+
+        CfnTopicRule.TopicRulePayloadProperty topicRulePayloadProperty = CfnTopicRule.TopicRulePayloadProperty.builder()
+                .actions(singletonList(actionProperty))
+                .ruleDisabled(false)
+                .sql(sql)
+                .awsIotSqlVersion(AWS_IOT_SQL_VERSION)
+                .build();
+
+        CfnTopicRuleProps cfnTopicRuleProps = CfnTopicRuleProps.builder()
+                .topicRulePayload(topicRulePayloadProperty)
+                .build();
+
+        return new CfnTopicRule(stack, topicRuleName, cfnTopicRuleProps);
+    }
+
+    public static CfnTopicRule buildIotEventRule(Stack stack, String topicRuleName, Function lambda, String selectClause, String topicFilter) {
+        String quotedTopicFilter = "'" + topicFilter + "'";
+        String sql = String.join(" ", selectClause, "from", quotedTopicFilter);
+
+        return buildIotEventRule(stack, topicRuleName, lambda, sql);
+    }
+
+    public static CfnTopicRule buildIotEventRule(Stack stack, String topicRuleName, String sql, Function lambda) {
+        CfnTopicRule.ActionProperty actionProperty = CfnTopicRule.ActionProperty.builder()
+                .lambda(CfnTopicRule.LambdaActionProperty.builder()
+                        .functionArn(lambda.getFunctionArn())
+                        .build())
+                .build();
+
+        CfnTopicRule.TopicRulePayloadProperty topicRulePayloadProperty = CfnTopicRule.TopicRulePayloadProperty.builder()
+                .actions(singletonList(actionProperty))
+                .ruleDisabled(false)
+                .sql(sql)
+                .awsIotSqlVersion(AWS_IOT_SQL_VERSION)
+                .build();
+
+        CfnTopicRuleProps cfnTopicRuleProps = CfnTopicRuleProps.builder()
+                .topicRulePayload(topicRulePayloadProperty)
+                .build();
+
+        return new CfnTopicRule(stack, topicRuleName, cfnTopicRuleProps);
+    }
+
     public static CfnTopicRule buildSelectAllIotEventRule(Stack stack, String topicRuleNamePrefix, Function lambda, String topic) {
         return RulesEngineSqlHelper.buildIotEventRule(stack, topicRuleNamePrefix + "TopicRule", lambda, "select * from '" + topic + "'");
     }
