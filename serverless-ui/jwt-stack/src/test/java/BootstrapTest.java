f75a70df02f0b478f8029e1317707c0ed42b9da4
Tim Mattison <tim@mattison.org>
2021-02-22 11:06:28 -0500

    Minor cleanup

--- serverless-ui/jwt-stack/src/test/java/BootstrapTest.java
+++ serverless-ui/jwt-stack/src/test/java/BootstrapTest.java
@@ -7,21 +7,20 @@ import java.io.File;
 import java.security.KeyPair;
 
 public class BootstrapTest {
-    public static final String RESOURCES_DIRECTORY = "src/main/resources/";
+    public static final String RESOURCES_DIRECTORY = "src/main/resources";
     public static final String KEY_PREFIX = "fixed";
-    public static final String FULL_KEY_PREFIX = String.join("", RESOURCES_DIRECTORY, KEY_PREFIX);
-    public static final Logger log = LoggerFactory.getLogger(BootstrapTest.class);
+    public static final String FULL_KEY_PREFIX = String.join("/", RESOURCES_DIRECTORY, KEY_PREFIX);
 
     /**
      * This test bootstraps the fixed keypair
      */
     @Test
     public void bootstrapFixedKeys() {
-        File publicKeyFile = new File(FULL_KEY_PREFIX + "-public.key");
-        File privateKeyFile = new File(FULL_KEY_PREFIX + "-private.key");
+        File publicKeyFile = new File(String.join("-", FULL_KEY_PREFIX, "public.key"));
+        File privateKeyFile = new File(String.join("-", FULL_KEY_PREFIX, "private.key"));
 
         if (publicKeyFile.exists() && privateKeyFile.exists()) {
-            log.info("Keys already exist, skipping creation");
+            // Keys already exist, nothing to do here
             return;
         }
 
