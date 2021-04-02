4ec9770c70f0666df26c5b28a4bb9e417e89a058
Tim Mattison <tim@mattison.org>
2021-04-02 13:33:22 -0400

    Fixed Ion stack to use ATS endpoints

--- amazon-ion-handler/Ion.py
+++ amazon-ion-handler/Ion.py
@@ -6,10 +6,14 @@ import os
 import boto3
 import base64
 
-iot_data_client = boto3.client('iot-data')
+iot = boto3.client('iot')
+endpoint_address = iot.describe_endpoint(endpointType='iot:Data-ATS')['endpointAddress']
+iot_data = boto3.client('iot-data', endpoint_url='https://' + endpoint_address)
+
 output_topic = os.environ['OutputTopic']
 
-def function_handler(event, context): 
-  decoded = base64.b64decode(event['data'])
-  obj = ion.loads(decoded)
-  return iot_data_client.publish(topic = output_topic, qos = 0, payload = json.dumps(dict(obj.items())))
+
+def function_handler(event, context):
+    decoded = base64.b64decode(event['data'])
+    obj = ion.loads(decoded)
+    return iot_data.publish(topic=output_topic, qos=0, payload=json.dumps(dict(obj.items())))
--- amazon-ion-handler/Json.py
+++ amazon-ion-handler/Json.py
@@ -4,8 +4,12 @@ import amazon.ion.simpleion as ion
 import os
 import boto3
 
-iot_data_client = boto3.client('iot-data')
+iot = boto3.client('iot')
+endpoint_address = iot.describe_endpoint(endpointType='iot:Data-ATS')['endpointAddress']
+iot_data = boto3.client('iot-data', endpoint_url='https://' + endpoint_address)
+
 output_topic = os.environ['OutputTopic']
 
-def function_handler(event, context): 
-  return iot_data_client.publish(topic = output_topic, qos = 0, payload = ion.dumps(event, binary=True))
+
+def function_handler(event, context):
+    return iot_data.publish(topic=output_topic, qos=0, payload=ion.dumps(event, binary=True))
