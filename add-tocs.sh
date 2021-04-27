fa258b7342581bd5b8ede86fc614f1cabc25dd69
Tim Mattison <tim@mattison.org>
2021-04-27 09:08:12 -0400

    Add TOC script now always installs the dependencies

--- add-tocs.sh
+++ add-tocs.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 # Requires markdown-toc -- npm install -g markdown-toc
+sudo npm install -g markdown-toc
 # Requires rename -- brew install rename
+brew install rename
 find . -name "*.without-toc.md" -exec sh -c "cp {} {}-processed.md && markdown-toc -i {}-processed.md && rename -f 's/without-toc.md-processed.md/md/' '{}-processed.md'" \;
