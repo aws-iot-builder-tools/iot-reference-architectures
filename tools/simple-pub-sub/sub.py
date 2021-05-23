b7cdfe21aa57bd689b14773d6aaaaf26732907ae
Tim Mattison <tim@mattison.org>
2021-05-23 13:44:01 -0400

    Added MESSAGE_LIMIT environment variable

--- tools/simple-pub-sub/sub.py
+++ tools/simple-pub-sub/sub.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 
+import datetime
+import os
+import sys
 from shared import argparse
 from shared import setup
 from shared import sleep
-import datetime
+
+try:
+    MESSAGE_LIMIT = int(os.getenv('MESSAGE_LIMIT'))
+except ValueError as ex:
+    MESSAGE_LIMIT = None
+
+MESSAGE_COUNT = 0
+RUNNING = True
 
 parser = argparse.ArgumentParser(description='Subscribe to a topic or topics')
 parser.add_argument('topics', nargs='+',
@@ -17,8 +27,24 @@ if not isinstance(topics, list):
 print('Subscribing to ' + ', '.join(topics))
 
 
+def disconnect_and_exit():
+    global RUNNING
+    global mqtt_client
+
+    RUNNING = False
+    mqtt_client.disconnect()
+    sys.exit(0)
+
+
 def print_payload(client, userdata, message):
+    global MESSAGE_COUNT
+    global MESSAGE_LIMIT
+
     print(str(datetime.datetime.now()) + ' - ' + str(message.topic) + ' - ' + str(message.payload))
+
+    MESSAGE_COUNT = MESSAGE_COUNT + 1
+    if (MESSAGE_LIMIT is None): return
+    if (MESSAGE_COUNT >= MESSAGE_LIMIT): disconnect_and_exit()
 
 
 mqtt_client = setup()
@@ -27,5 +53,5 @@ mqtt_client.connect()
 for topic in topics:
     mqtt_client.subscribe(topic, 0, print_payload)
 
-while True:
+while RUNNING:
     sleep(1)
