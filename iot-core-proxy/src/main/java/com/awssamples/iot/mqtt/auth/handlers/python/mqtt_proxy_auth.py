e3cb31907713a1f8513ba8a15b8ef8959a06306d
Charlton <wchrlt@147dda2e9ded.ant.amazon.com>
2021-09-21 00:20:28 -0500

    simplify lambda auth

--- iot-core-proxy/src/main/java/com/awssamples/iot/mqtt/auth/handlers/python/mqtt_proxy_auth.py
+++ iot-core-proxy/src/main/java/com/awssamples/iot/mqtt/auth/handlers/python/mqtt_proxy_auth.py
@@ -1,22 +1,10 @@
-import os
 import json
-import base64
 
-account = os.environ['ACCOUNT']
-region = os.environ['REGION']
 
 def handler(event, context):
-    # https://docs.aws.amazon.com/iot/latest/developerguide/config-custom-auth.html#custom-auth-lambda
-    print(f'event: json.dumps(event)')
-    username = event['protocolData']['mqtt']['username']
-    password = base64.b64decode(event['protocolData']['mqtt']['password'].encode('ascii')).decode('ascii')
-    clientId = event['protocolData']['mqtt']['clientId']
-    print(f'username: {username}')
-    # print(f'password: {password}')
-    print(f'clientId: {clientId}')
     policy = {
         "isAuthenticated": True,
-        "principalId": f"{clientId}",
+        "principalId": "*",
         "disconnectAfterInSeconds": 86400,
         "refreshAfterInSeconds": 300,
         "policyDocuments": [
@@ -24,24 +12,9 @@ def handler(event, context):
                 "Version": "2012-10-17",
                 "Statement": [
                     {
-                        "Action": "iot:Connect",
+                        "Action": "iot:*",
                         "Effect": "Allow",
-                        "Resource": f"arn:aws:iot:{region}:{account}:client/*"
-                    },
-                    {
-                        "Action": "iot:Publish",
-                        "Effect": "Allow",
-                        "Resource": f"arn:aws:iot:{region}:{account}:topic/*"
-                    },
-                    {
-                        "Action": "iot:Subscribe",
-                        "Effect": "Allow",
-                        "Resource": f"arn:aws:iot:{region}:{account}:topicfilter/*"
-                    },
-                    {
-                        "Action": "iot:Receive",
-                        "Effect": "Allow",
-                        "Resource": f"arn:aws:iot:{region}:{account}:topic/*"
+                        "Resource": "*"
                     }
                 ]
             }
