873f24dfd5bc4e1cb3999e6c32d3e590bd31345e
Tim Mattison <tim@mattison.org>
2021-10-19 13:32:34 -0400

    Python code now takes the message as a parameter

--- cross-account-publish/certificate-based-stack/python/publish.py
+++ cross-account-publish/certificate-based-stack/python/publish.py
@@ -16,6 +16,9 @@ parser.add_argument('--cert', help='The file that contains the certificate', typ
 parser.add_argument('--key',
                     help='The file that contains the private key. Optional if the certificate key\'s filename ends in \'.key\' and is otherwise the same as the certificate\'s filename (e.g. iot.pem and iot.key).',
                     type=ascii)
+parser.add_argument('--message',
+                    help='The message that should be published. This is a string and will sent exactly as specified. It is not automatically converted to JSON.',
+                    type=ascii)
 
 args = parser.parse_args()
 
@@ -27,6 +30,9 @@ if args.key is None:
     extension_index = args.key.rindex(".")
     args.key = args.key[:extension_index] + ".key"
 
+if args.message is None:
+    raise Exception("The message is a required argument")
+
 args.cert = args.cert.replace("'", "")
 args.key = args.key.replace("'", "")
 
@@ -37,11 +43,9 @@ topic = cert.get_subject().CN.replace("%2F", "/")
 
 publish_url = 'https://' + endpoint + ':8443/topics/' + topic + '?qos=1'
 
-def cross_account_publish(payload):
+def cross_account_publish(message):
     headers = {'x-amzn-platform': 'APN/1 partner,solution'}
     
-    publish = requests.request('POST', publish_url, data=json.dumps(payload), cert=[args.cert, args.key], headers=headers)
+    publish = requests.request('POST', publish_url, data=message, cert=[args.cert, args.key], headers=headers)
 
-payload = {}
-payload['message'] = "Hello, world!"
-cross_account_publish(payload)
+cross_account_publish(args.message)
