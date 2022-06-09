8c6ba0ea1bd87b4820605ff6c4019ad8294a1339
Tim Mattison <tim@mattison.org>
2022-06-09 09:22:36 -0400

    Fixed jwt-stack
    Some bad dependency updates were preventing this stack from being buildable. They have been fixed.

--- serverless-ui/jwt-stack/src/main/java/com/awssamples/server/Injector.java
+++ serverless-ui/jwt-stack/src/main/java/com/awssamples/server/Injector.java
@@ -3,6 +3,9 @@ package com.awssamples.server;
 import com.awslabs.resultsiterator.ResultsIteratorModule;
 import dagger.Component;
 
+import javax.inject.Singleton;
+
+@Singleton
 @Component(modules = ResultsIteratorModule.class)
 public interface Injector {
     void inject(BasicJwtService basicJwtService);
